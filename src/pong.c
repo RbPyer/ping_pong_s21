e34f37ee8178cf19aa37f6e7c4a02032c89c95cc
Oswyn Deloras <oswyndel@su-g4.msk.21-school.ru>
2023-06-25 15:28:07 +0300

    move rockets

--- src/pong.c
+++ src/pong.c
@@ -28,69 +28,84 @@ void print_score(short int player_score1, short int player_score2) {
 }
 
 short int check_score(short int player_score1, short int player_score2) {
+    short int flag = 0;
     if (player_score1 == max_count || player_score2 == max_count) {
         printf("%45s", "Game over!\n");
         player_score1 > player_score2 ? printf("%47s", "Player 1 wins!\n")
                                       : printf("%47s", "Player 2 wins!\n");
-        return 1;
+        flag = 1;
     }
-    return 0;
+    return flag;
 }
 
 short int print_lracket(const int row, const int col, const int coord) {
+    short int flag = 0;
     if (col == 2 && (coord == row || coord == row - 1 || coord == row + 1)) {
         renderRakets();
-        return 1;
+        flag = 1;
     }
-    return 0;
+    return flag;
 }
 
 short int print_rracket(const int row, const int col, const int coord) {
+    short int flag = 0;
     if (col == width - 4 && (coord == row || coord == row - 1 || coord == row + 1)) {
         renderRakets();
-        return 1;
+        flag = 1;
     }
-    return 0;
+    return flag;
 }
 
-void print_field() {
-    short int ballX = 39, ballY = 13;
-    short int lRacket = 16, rRacket = 8;
-    short int ballDw = -1, ballDh = 1;
-    short int p1_score = 0, p2_score = 0;
-    char input;
-    while (1) {
-        if (check_score(p1_score, p2_score) == 1) {
-            break;
+void pass_width(short int el_h, short int ballX, short int ballY, short int lRacket, short int rRacket) {
+    for (short int el_w = 0; el_w < width; el_w++) {
+        if (ballX == el_w && ballY == el_h) {
+            renderBall();
+        } else if (el_h == 0 || el_h == heigth - 1) {
+            print_border_y();
+        } else if (el_w == 0 || el_w == width - 1) {
+            print_border_x();
+        } else {
+            if ((print_lracket(el_h, el_w, lRacket) == 1)) {
+                el_w = el_w + 1;
+            };
+            if ((print_rracket(el_h, el_w, rRacket) == 1)) {
+                el_w = el_w + 1;
+            };
+            renderEmpty();
         }
+    }
+}
+
+
+void print_field() {
+    short int ballX = 39, ballY = 13, lRacket = 16, rRacket = 8, ballDw = -1, ballDh = 1, p1_score = 0,
+              p2_score = 0;
+    char input;
+    while (check_score(p1_score, p2_score) != 1) {
         for (short int el_h = -1; el_h < heigth; el_h++) {
             if (el_h == -1) {
                 print_score(p1_score, p2_score);
                 continue;
             }
-            for (short int el_w = 0; el_w < width; el_w++) {
-                if (ballX == el_w && ballY == el_h) {
-                    renderBall();
-                } else if (el_h == 0 || el_h == heigth - 1) {
-                    print_border_y();
-                } else if (el_w == 0 || el_w == width - 1) {
-                    print_border_x();
-                } else {
-                    if ((print_lracket(el_h, el_w, lRacket) == 1)) {
-                        el_w = el_w + 1;
-                    };
-                    if ((print_rracket(el_h, el_w, rRacket) == 1)) {
-                        el_w = el_w + 1;
-                    };
-                    renderEmpty();
-                }
-            }
+            pass_width(el_h, ballX, ballY, lRacket, rRacket);
             putchar('\n');
         }
         scanf("%c", &input);
-        if (input == 'a') {
-            ballX = ballX - 1;
+        switch (input) {
+            case 'a':
+                (lRacket < 3) ? lRacket : lRacket--;
+                break;
+            case 'z':
+                (lRacket > 21) ? lRacket : lRacket++;
+                break;
+            case 'k':
+                (rRacket < 3) ? rRacket : rRacket--;
+                break;
+            case 'm':
+                (rRacket > 21) ? rRacket : rRacket++;
+                break;
         }
+
         if (ballX + ballDw == 0 || ballX + ballDw == width - 1) {
             if (ballX + ballDw == 0) {
                 p2_score = p2_score + 1;
@@ -109,6 +124,7 @@ void print_field() {
 }
 
 int main() {
+    clear_screen();
     print_field();
     return 0;
 }
